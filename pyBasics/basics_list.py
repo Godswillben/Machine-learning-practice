985baa3a9161011a928fa44b73875869fea5aa48
Godswillben <amogugodswill99@gmail.com>
2023-01-27 16:05:47 +0100

    basic types. Fundamentals

--- pyBasics/basics_list.py
+++ pyBasics/basics_list.py
@@ -23,3 +23,32 @@ matrix = [
 ]
 
 print(matrix[0][1])
+
+basket = [1, 2, 3, 4, 5]
+
+# list methods
+
+# adding
+basket.append(100)
+new_list = basket
+print(basket)
+print(new_list)
+
+# insert
+basket.insert(5, 100)
+new_list = basket
+print(basket)
+print(new_list)
+
+# adding
+basket.extend([5, 100])
+new_list = basket
+print(basket)
+print(new_list)
+
+# removing
+basket.pop()
+basket.pop(0)
+basket.remove(3)
+basket.clear()
+print(basket)
