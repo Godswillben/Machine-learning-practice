6940f507688fc13f322814245950194dfb89c269
Godswillben <amogugodswill99@gmail.com>
2023-01-27 14:16:38 +0100

    basic types. Fundamentals

--- pyBasics/basic_exer.py
+++ pyBasics/basic_exer.py
@@ -4,3 +4,12 @@ birth_year = input('what year were you born?')
 age = 2019 - int(birth_year)
 
 print(f'your age is: {age}')
+
+# python basics part one: excersice
+username = input('what is your username?')
+password = input('what is your password?')
+
+password_length = len(password)
+hidden_password = '*' * password_length
+
+print(f'{username}, your password, {hidden_password}, is {password_length} characters long')
