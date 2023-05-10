794cbf3768f07aa75d59e63465d4f0237d7f251c
Godswillben <amogugodswill99@gmail.com>
2023-05-10 17:19:26 +0100

    python basics 2

--- 2/pybasics/main.py
+++ 2/pybasics/main.py
@@ -1,1 +1,13 @@
 # Data type fundamentals
+
+# Dictionary
+dictionary = {
+    "weapons":[1, 2, 3],
+    "greating": 'hello',
+    "is_Magic": True
+}
+
+my_list = ['lkdsjlj']
+
+print(my_list[0][2])
+print(dictionary['a'][1])
