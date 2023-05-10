82e5a3d364d2ba2a5d78eb271ecff6fc32e8fae6
Godswillben <amogugodswill99@gmail.com>
2023-05-10 17:29:54 +0100

    python basics 2

--- 2/pybasics/main.py
+++ 2/pybasics/main.py
@@ -9,5 +9,14 @@ dictionary = {
 
 my_list = ['lkdsjlj']
 
-print(my_list[0][2])
-print(dictionary['a'][1])
+# print(my_list[0][2])
+# print(dictionary['a'][1])
+
+# Tuple
+my_tuple = (1,2,3,4,5)
+print(5 in my_tuple)
+print ("p" in "apple")
+user = {
+    (1,2): [1,2,3],
+    'greet': "hello",
+}
