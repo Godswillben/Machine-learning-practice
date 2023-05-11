8d7f335df15265781468149939bdee531813adf1
Godswillben <amogugodswill99@gmail.com>
2023-05-11 18:56:45 +0100

    pybasics

--- 2/pybasics/main.py
+++ 2/pybasics/main.py
@@ -20,3 +20,14 @@ user = {
     (1,2): [1,2,3],
     'greet': "hello",
 }
+
+# conditionals 
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+
+print('okoko')
