91396817814049daca508be5109e866e4089f2cf
Godswillben <amogugodswill99@gmail.com>
2023-05-11 20:05:53 +0100

    pybasics

--- 2/pybasics/indentation.py
+++ 2/pybasics/indentation.py
@@ -5,3 +5,24 @@ is_friend = False
 can_message = "message allowed" if is_friend else "allowed to message"
 
 print(can_message)
+
+# Short Circuiting
+is_friend = False
+is_user = True
+
+if is_friend and is_user:   # if first check results in true it jumps to conditonal body.
+    print('best friends forever')
+
+# is vs ==
+print( True == 1)
+print('1' == 1)
+print([] == 1)
+print(10 == 10.0)
+print([1,2,3,] == [1,2,3])
+
+
+print( True is 1)
+print('1' is 1)
+print([] is 1)
+print(10 is 10.0)
+print([1,2,3,] is [1,2,3])
