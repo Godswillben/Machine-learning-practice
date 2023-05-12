21f2cc6f7ecdf9b7f7506d58ac0f2cea61bec277
Godswillben <amogugodswill99@gmail.com>
2023-05-12 10:18:02 +0100

    pybasics

--- 2/pybasics/iterable.py
+++ 2/pybasics/iterable.py
@@ -24,3 +24,17 @@ while True:
     response = input('say something!: ')
     if (response == 'bye'):
         break
+
+# continue, pass, break
+my_list = [1,2,3]
+for item in my_list:
+    # thinking about it
+    pass
+
+i=0
+
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+    pass
+
