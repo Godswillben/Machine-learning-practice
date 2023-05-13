ea2918dbf6360f6db48bdf5b86bca41d6f83e81a
Godswillben <amogugodswill99@gmail.com>
2023-05-13 21:27:58 +0100

    pybasics

--- 2/pybasics/functions.py
+++ 2/pybasics/functions.py
@@ -108,3 +108,9 @@ def multiply_by2(li):
 
 print(multiply_by2([1,2,3]))
 
+# map, filter, zip, and reduce
+my_list = [1,2,3]
+def multiply_by2(item):
+    return item*2
+
+print(list(map(multiply_by2, [1,2,3])))
