b94e6b6520884f85f08dc50841f7e492d85654a0
Godswillben <amogugodswill99@gmail.com>
2023-05-14 16:14:43 +0100

    pybasics

--- 2/pybasics/comprehension.py
+++ 2/pybasics/comprehension.py
@@ -16,3 +16,12 @@ my_list4 = {num**2 for num in range(0, 100) if num % 2 == 0}
 
 print(my_list4)
 
+# dictionary
+simple_dict = {
+    'a': 1,
+    'b': 2,
+}
+
+my_dict = {k:v**2 for k,v in simple_dict.items() if v%2==0}
+
+print(my_dict)
