5b902debabf31a620b926bcfe9a6a2871a6418a9
Godswillben <amogugodswill99@gmail.com>
2024-10-05 20:08:07 +0100

    py basics

---
+++ 3/pyBasics/main.py
@@ -0,0 +1,48 @@
+print(type("hi hello there 24"))
+username = 'supercoder'
+password = 'supersecret'
+
+hell0=123
+print(f'hi {name}. you are {age} years old')
+
+selfish = '0123456'
+print(selfish[::-1])
+
+matraxes = [
+    [1,2,3,],
+    [1,2,1,]
+]
+
+len(matraxes)
+
+new_list = matraxes.append(100)
+
+a,b,c,*d = [1,2,3,4,6]
+
+# dictionary
+dictionary = {
+    'a': [1,2,3],
+    'b': 'hell',
+    'x': True
+}
+
+my_list = [
+    {
+        'a':[1,2,3],
+        'b':'hello',
+        'x':True
+    },
+    {
+        'a':[5,7,4],
+    }
+]
+
+print(dictionary.get('x'))
+
+# Tuple 
+my_tuple = (1,2,3,4,5)
+print(5 in my_tuple)
+
+# sets
+my_set = {1,2,3,4,5}
+your_set = {4,5,6}
